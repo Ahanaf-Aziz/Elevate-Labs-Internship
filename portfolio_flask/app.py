0e82a16ced6b23820984e7aa41c664ceefd4daa1
Ahanaf Aziz <148462115+Ahanaf-Aziz@users.noreply.github.com>
2025-09-30 17:17:16 +0530

    Initialize Flask app with contact form routes

---
+++ portfolio_flask/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        message = request.form.get('message')
+        # Here, you could add code to send email or save to a database
+        return redirect(url_for('thankyou'))
+    return render_template('contact.html')
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
